b5bb971e525301a8407ae23868bc25ff129cd92e
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2020-11-11 01:07:37 +0100

    changed

--- projekt-zal/src/App.js
+++ projekt-zal/src/App.js
@@ -1,18 +1,30 @@
-import './sass/reset.scss'
-import './App.scss';
 
-import Navbar from './components/Navbar'
-import {BrowserRouter as Router} from 'react-router-dom';
-
-
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from '../src/components/pages/SingUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+import './App.css'
 function App() {
   return (
-    <>
     <Router>
-    <Navbar/>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/contact-us' component={ContactUs} />
+        <Route path='/projekt-zal/src/components/pages/SingUp' component={SignUp} />
+        <Route path='/marketing' component={Marketing} />
+        <Route path='/consulting' component={Consulting} />
+      </Switch>
     </Router>
-    </>
   );
 }
 
