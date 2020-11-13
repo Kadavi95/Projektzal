06fdb729d68d4002418c1e88ed4a6c142f7c090e
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2020-11-13 02:36:44 +0100

    validated

--- projekt-zal/src/components/pages/appointment.js
+++ projekt-zal/src/components/pages/appointment.js
@@ -3,11 +3,34 @@ import '../../App.css';
 import {useForm} from 'react-hook-form'
 
 export default function Appoint() {
+
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  
+ 
+
   return (
     <div className='container'>
       <h1 className='formTitle'>Umów wizytę</h1>
-      <form>
-      <label>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <label> 
+        Podaj swoje imię
+      <input type="text" name="name" ref={register({ required: true })}></input>
+      <p>{errors.name && <span>To pole jest wymagane</span>}</p>
+      </label>
+      <label> 
+        Podaj swoje nazwisko
+      <input type="text" name="surname" ref={register({ required: true })}></input>
+      {errors.surname && <span>To pole jest wymagane</span>}
+      </label>
+      <label> 
+        Podaj swój nr telefonu
+      <input type="text" name="phone" ref={register({ required: true })}></input>
+      {errors.phone && <span>To pole jest wymagane</span>}
+      </label>
+      <input type="submit" />
+    
+      {/* <label>
             <p>Wpisz swoje imię</p>
             <input name="name"/>
       </label>
@@ -73,7 +96,7 @@ export default function Appoint() {
       <p>zapoznałem się z treścią klauzuli RODO</p>
       <input type="checkbox"/>
       <button type="submit">Submit</button>
-      <h3>Informujemy, że potwierdzenie wizyty przyjdzie do Państwa drogą telefoniczną </h3>
+      <h3>Informujemy, że potwierdzenie wizyty przyjdzie do Państwa drogą telefoniczną </h3> */}
       </form>
     </div>
   )
