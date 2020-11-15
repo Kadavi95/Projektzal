f356f50d1dcc14f2bda4261f34f58cd5d45dad81
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2020-11-15 03:08:20 +0100

    started styling

--- projekt-zal/src/components/pages/ContactUS.js
+++ projekt-zal/src/components/pages/ContactUS.js
@@ -1,36 +1,39 @@
 import React, {useState} from 'react';
 import '../../App.css';
 import {useForm} from 'react-hook-form'
-import { firestore } from './firebase'
-
 
 export default function ContactUs() {
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = data => console.log(data);
 
+  // const Contact = () => {
+  //   const [email, setEmail] = useState("");
+  //   const [message, setMessage] = useState("")
+  //   const [rodo, setRodo] = useState("")
+  // }
 
-  
+
   return (
     <div className='container'>
       <div className='date'>
-        <h1>Przychodnia VET</h1>
-        <p>Adres: ul. Pieskowa 13b, 10-001 Pieskowo Górne</p>
-        <p>Numer tel: 11 333 222 11</p>
-        <p>Adres mail: pieskigórą@pieski.pl</p>
-        <p>Nr NIP: 313-1313-22-11</p>
-        <p>KRS: 313- 222 222</p>
+        <h1 className="date_heading">Przychodnia VET</h1>
+        <p className="date_text">Adres: ul. Pieskowa 13b, 10-001 Pieskowo  Górne</p>
+        <p className="date_text">Numer tel: 11 333 222 11</p>
+        <p className="date_text">Adres mail: pieskigórą@pieski.pl</p>
+        <p className="date_text">Nr NIP: 313-1313-22-11</p>
+        <p className="date_text">KRS: 313- 222 222</p>
       </div>
       <div className='date-active'>
         <h1>Napisz do nas wiadomość</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <label> 
             Podaj swój adres email:
-            <input type="text" name="email" ref={register({ required: true })}></input>
+            <input placeholder="Email"type="text" name="email" ref={register({ required: true })}></input>
             <p>{errors.email && <span>To pole jest wymagane</span>}</p>
           </label>
           <label> 
             Wpisz treść wiadmości:
-            <textarea type='textarea' name='textarea'ref={register({ required: true })} ></textarea>
+            <textarea placeholder="Message"type='textarea' name='textarea'ref={register({ required: true })} ></textarea>
             <p>{errors.textarea && <span>To pole jest wymagane</span>}</p>
           </label>
           <label>
--- projekt-zal/src/components/pages/appointment.js
+++ projekt-zal/src/components/pages/appointment.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../App.css';
+import './appointment.scss'
 import {useForm} from 'react-hook-form'
 
 export default function Appoint() {
@@ -11,65 +11,65 @@ export default function Appoint() {
     <div className='container'>
       <h1 className='formTitle'>Umów wizytę</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form className='container_form'onSubmit={handleSubmit(onSubmit)}>
         <label> 
           Podaj swoje imię
-          <input type="text" name="name" ref={register({ required: true })}></input>
+          <input className='form_name' placeholder="name" type="text" name="name" ref={register({ required: true })}></input>
           <p>{errors.name && <span>To pole jest wymagane</span>}</p>
         </label>
 
         <label> 
           Podaj swoje nazwisko
-          <input type="text" name="surname" ref={register({ required: true })}></input>
+          <input className='form_surname' placeholder="surname" type="text" name="surname" ref={register({ required: true })}></input>
           <p>{errors.surname && <span>To pole jest wymagane</span>}</p>
         </label>
 
         <label> 
           Podaj swój nr telefonu
-          <input type="number" name="phone" ref={register({ required: true })}></input>
+          <input className='form_number'placeholder="lelephone number" type="number" name="phone" ref={register({ required: true })}></input>
           {errors.phone && <span>To pole jest wymagane</span>}
         </label>
 
         <label> 
           Wybierz lekarza
-          <select name="doctor" ref={register}>
-            <option>Dr.Predator</option>
-            <option>Dr.Kowalski</option>
-            <option>Dr.Nowak</option>
+          <select className='form_select-doc'name="doctor" ref={register}>
+            <option className='form_dec_-option'>Lek. wet. Predator</option>
+            <option className='form_dec_-option'>Lek. wet. Kowalski</option>
+            <option className='form_dec_-option'>Lek. wet. Nowak</option>
           </select>
         </label>
 
         <label> 
           Wybierz interesujący Cię dzień
-          <select name="day" ref={register}>
-            <option>Poniedziałek</option>
-            <option>Wtorek</option>
-            <option>Środa</option>
-            <option>Czwartek</option>
-            <option>Piątek</option>
+          <select className='form_select-day' name="day" ref={register}>
+            <option className='form_day-option'>Poniedziałek</option>
+            <option className='form_day-option'>Wtorek</option>
+            <option className='form_day-option'>Środa</option>
+            <option className='form_day-option'> Czwartek</option>
+            <option className='form_day-option'>Piątek</option>
           </select>
         </label>
 
         <label> 
           Wybierz interesującą Cię godzinę
-          <select name="hour" ref={register}>
-            <option>8:00</option>
-            <option>9:00</option>
-            <option>10:00</option>
-            <option>11:00</option>
-            <option>12:00</option>
-            <option>13:00</option>
-            <option>14:00</option>
-            <option>15:00</option>
-            <option>16:00</option>
+          <select className='form_select-hour' name="hour" ref={register}>
+            <option className='form_day-option'>8:00</option>
+            <option className='form_day-option' >10:00</option>
+            <option className='form_day-option' >9:00</option>
+            <option className='form_day-option' >11:00</option>
+            <option className='form_day-option' >12:00</option>
+            <option className='form_day-option' >13:00</option>
+            <option className='form_day-option' >14:00</option>
+            <option className='form_day-option' >15:00</option>
+            <option className='form_day-option' >16:00</option>
           </select>
         </label>
         <label>
           Zapoznałem się z klauzulą RODO
-            <input type='checkbox' name='rodo' ref={register({ required: true })}></input>
+            <input className='form_checkbox' type='checkbox' name='rodo' ref={register({ required: true })}></input>
             {errors.rodo && <span>To pole jest wymagane</span>}
         </label>
-      <input type="submit" />
+      <input className='form_button'type="submit" />
       </form>
       <h1>Informujemy, że wszelkie wizyty potwierdzamy drogą telefoniczną</h1>
     </div>
